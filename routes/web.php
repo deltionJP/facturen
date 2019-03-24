fbf46e1e357780c242b827aa118f52a412787320
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-03-24 10:51:36 +0100

    invoice and products change

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,11 @@ Route::get('/facturen','InvoiceController@index');
 Route::get('/','InvoiceController@index');
 Route::post('/facturen/create','InvoiceController@createInvoice')->name('facturen.create');
 Route::get('/facturen/{factuurnummer}/edit','InvoiceController@edit')->name('facturen.edit');
+Route::get('/facturen/{factuurnummer}/product/add','InvoiceController@addInvoiceProduct')->name('facturen.add_product'); //product toevoegen
+
 Route::get('/downloadPDF/{factuurnummer}','InvoiceController@downloadPDF');
 
 // Route::get('/facturen/pdf','InvoiceController@downloadPDF')->name('facturen.pdf');
 Route::post('/product','ProductController@create')->name('product.create');
-// Route::post('/product','ProductController@delete')->name('product.delete');
+// Route::delete('/product/delete/{id}','ProductController@delete')->name('product.delete');
 Route::get('/product','ProductController@index');
