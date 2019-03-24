bb241b1da2006a3171c47baffbe074cf8feea374
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-03-24 12:26:30 +0100

    change layout

--- resources/views/factuur/overzicht.blade.php
+++ resources/views/factuur/overzicht.blade.php
@@ -10,18 +10,19 @@
 
 
     <section class="content">
-        <div class="container">
+        {{-- <div class="container"> --}}
             <div class="row justify-content-center">
-                <div class="col-md-12">
+                <div class="col-md-8" style="background-color: #fff; color: black;">
                     <div class="card" style="">
                         <div class="card-header">
-                            {{-- <h3 class="card-title">Bordered Table</h3> --}}
+                            <h3 class="card-title"></h3>
                         </div>
                         <div class="card-body">
                             <table class="table table-bordered table-striped">
                                 <tr>
                                     <th>#</th>
                                     <th>#</th>
+                                    <th>Factuurnummer</th>
                                     <th>Factuurdatum</th>
                                     <th>Klant</th>
                                     <th>Betaald?</th>
@@ -32,6 +33,7 @@
                                         <td><a href="{{ route('facturen.add_product', $factuur->factuurnummer) }}">Product toevoegen</a></td>
                                         <td><a href="{{ route('facturen.edit', $factuur->factuurnummer) }}">Instellingen</a></td>
                                         {{-- <td><a href="{{ route('zfacturen.edit', $factuur->factuurnummer) }}">{{$factuur->factuurnummer}}</td> --}}
+                                        <td>{{$factuur->factuurnummer}}</td>
                                         <td>{{$factuur->factuurdatum}}</td>
                                         <td>{{$factuur->hasCustomer->voorletter}}. {{$factuur->hasCustomer->achternaam}}</td>
                                         @if($factuur->betaald == 0)
@@ -44,17 +46,58 @@
                                     </tr>
                                 @endforeach
                             </table>
-                        </div>
+                         </div>
                     </div>
                 </div>
+                 <div class="col-md-3" style="background-color: #fff; color: black; margin-left: 10px;">
+                    <div class="card" style="">
+                        <div class="card-header">
+                            <h3 class="card-title">Factuur toevoegen</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-body">
+                     <form method="post" action="{{route('facturen.create')}}">
+                         @csrf
+                         <div class="form-group row">
+                            <div class="col-md-8">
+                                 <select class="form-control" name="customer_id" id="customer_id">
+                                     @if($customers->count() > 0)
+                                         @foreach($customers as $customer)
+
+                                         <option value="{{$customer->customer_id}}">{{$customer->achternaam}}</option>
+                                         @endForeach
+                                         @else
+                                         Geen klanten gevonden
+                                         @endif
+                                 </select>
+
+                                 @if ($errors->has('customer_id'))
+                                 <span class="invalid-feedback" role="alert">
+                                     <strong>{{ $errors->first('customer_id') }}</strong>
+                                 </span>
+                                 @endif
+                            </div>
+                         </div>
+                         <div class="form-group row">
+                            <div class="col-md-8">
+                                 <button type="submit" class="btn btn-success">Bestelling toevoegen</button>
+                            </div>
+                         </div>
+                     </form>
+
+                 </div>
+                         </div>
+                    </div>
+                </div>  
             </div>
-        </section>
+        {{-- </div> --}}
+    </section>
 
 
 
 
 
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -77,7 +120,6 @@
                                     <td><a href="{{ route('facturen.add_product', $factuur->factuurnummer) }}">Product toevoegen</a></td>
                                     <td><a href="{{ route('facturen.edit', $factuur->factuurnummer) }}">Instellingen</a></td>
 
-                                    {{-- <td><a href="{{ route('zfacturen.edit', $factuur->factuurnummer) }}">{{$factuur->factuurnummer}}</td> --}}
                                     <td>{{$factuur->factuurdatum}}</td>
                                     <td>{{$factuur->hasCustomer->voorletter}}. {{$factuur->hasCustomer->achternaam}}</td>
                                     @if($factuur->betaald == 0)
@@ -86,7 +128,6 @@
                                          <td>Ja</td>
                                     @endif
                                     <td><a href="{{action('InvoiceController@downloadPDF', $factuur->factuurnummer)}}">PDF</a></td>
-                                    {{-- <td><a href="{{route('facturen.pdf')}}">PDF</a></td> --}}
                                 </tr>
                             @endforeach
 
@@ -136,5 +177,5 @@
 
         </div>
     </div>
-</div>
+</div> --}}
 @stop
