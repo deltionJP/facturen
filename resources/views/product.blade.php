8907f7e2ff7e1d5bbbb69632c726a5128ac6768b
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-04-01 20:40:20 +0200

    change product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,99 +1,79 @@
-@extends('layouts.master')
+@extends('adminlte::page')
+
+@section('title', 'Facturen')
+
+@section('content_header')
+    <h1>Product overzicht</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Bestellingen van facturen</div>
 
-                <div class="card-body">
-                     <table class="table table-striped">
-                            <thead>
-                            <th></th>
-                            <th>Naam</th>
-                            <th>Omschrijving</th>
-                            {{-- <th></th> --}}
-                            {{-- <th>Prijs Per stuk</th> --}}
-                            </thead>
-                            <tbody>
-                            @foreach($producten as $product)
 
+    <section class="content">
+        {{-- <div class="container"> --}}
+            <div class="row justify-content-center">
+                <div class="col-md-8" style="background-color: #fff; color: black;">
+                    <div class="card" style="">
+                        <div class="card-header">
+                            <h3 class="card-title"></h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped">
                                 <tr>
-                                    <td>{{$product->id}}</td>
-                                    <td>{{$product->name}}</td>
-                                    <td>{{$product->description}}</td>
+                                    <th>#</th>
+                                    <th>Naam</th>
+                                    <th>Omschrijving</th>
                                 </tr>
-                            @endforeach
-
-                            </tbody>
-                        </table>
+                                @foreach($producten as $product)
+                                    <tr>
+                                        <td>{{$product->id}}</td>
+                                        <td>{{$product->name}}</td>
+                                        <td>{{$product->description}}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                         </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-             <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">Product toevoegen</h4>
-                    <form method="post" action="{{route('product.create')}}">
-                        @csrf
-
-                        {{-- <div class="form-group row">
-                            <div class="col-md-12">
-
-                                <select class="form-control" name="customer_id" id="customer_id">
-                                    @if($facturen->count() > 0)
-                                        @foreach($facturen as $factuur)
-
-                                            <option value="{{$factuur->factuurnummer}}">{{$factuur->factuurnummer}}/ {{$factuur->timestamp}}</option>
-                                        @endForeach
-                                    @else
-                                        Geen klanten gevonden
-                                    @endif
-                                </select>
-
-                                @if ($errors->has('customer_id'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('customer_id') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div> --}}
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                Naam:
-                                <input id="product_name" type="textarea" class="form-control"   name="product_name">
-                            </div>
+                <div class="col-md-3" style="background-color: #fff; color: black; margin-left: 10px;">
+                    <div class="card" style="">
+                        <div class="card-header">
+                            <h3 class="card-title">Product toevoegen</h3>
                         </div>
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                Omschrijving:
-                                <input id="product_description" type="textarea" class="form-control"   name="product_description">
-                            </div>
+                        <div class="card-body">
+                             <form method="post" action="{{route('product.create')}}">
+                             @csrf
+                                <div class="form-group row">
+                                    <div class="col-md-8">
+                                       <div class="form-group row">
+                                            <div class="col-md-12">
+                                                Naam:
+                                                <input id="product_name" type="textarea" class="form-control"   name="product_name">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-md-12">
+                                                Omschrijving:
+                                                <input id="product_description" type="textarea" class="form-control"   name="product_description">
+                                            </div>
+                                        </div>
+                                        {{-- @if ($errors->has('customer_id'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('customer_id') }}</strong>
+                                            </span>
+                                        @endif --}}
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-8">
+                                        <button type="submit" class="btn btn-success">Product toevoegen</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        {{-- <div class="form-group row">
-                            <div class="col-md-12">
-                                Prijs:
-                                <input id="prijsperstuk" type="text" class="form-control"   name="prijsperstuk">
-                            </div>
-                        </div> --}}
-                        {{-- <div class="form-group row">
-                            <div class="col-md-12">
-                                Aantal:
-                                <input id="aantal" type="number" class="form-control"   name="aantal">
-                            </div>
-                        </div> --}}
-
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-success">Product toevoegen</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                    </div>
+                {{-- </div> --}}
+            </div>  
+       </div>
+    </section>
+@stop
