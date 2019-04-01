069791272cd777306e7b9b3f257d403d7cd3ae5f
LAPTOP-G3F8C8DJ\Jordie <info@jordiepasman.nl>
2019-04-01 20:39:45 +0200

    install passport

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use Notifiable;
-    use HasRoles;
+    use HasRoles, HasApiTokens;
     protected $guard_name = 'web';
     /**
      * The attributes that are mass assignable.
